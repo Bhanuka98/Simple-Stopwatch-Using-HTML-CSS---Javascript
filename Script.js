e5dd9cfefbae0d4371ff073413b73228491d8127
Sachintha Ranasinghe <91958758+Bhanuka98@users.noreply.github.com>
2023-03-21 02:50:04 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,12 @@
+//let hours=00;
 let mins=00;
 let seconds=00;
 let tens=00;
+//let getHours = document.querySelector('.hours');
 let getMins = document.querySelector('.mins');
 let getSeconds = document.querySelector('.seconds');
 let getTens = document.querySelector('.tens');
+
 let btnStart = document.querySelector('.btn-start');
 let btnStop = document.querySelector('.btn-stop');
 let btnReset = document.querySelector('.btn-reset');
@@ -22,6 +25,9 @@ btnReset.addEventListener('click',()=>{
     tens= '00';
     seconds= '00';
     mins='00'
+    //hours='00'
+    
+    //getHours.innerHTML=hours;
     getMins.innerHTML=mins;
     getSeconds.innerHTML=seconds;
     getTens.innerHTML=tens;
@@ -50,7 +56,17 @@ function startTimer(){
         seconds=0;
         getSeconds.innerHTML='0'+0;
     }
-    if(mins >60){
+   if(mins >60){
         getMins.innerHTML=mins;
     }
+    
+    /*if(minss>=60){
+        hours++;
+        getHours.innerHTML='0'+hours;
+        mins=0;
+        getMinss.innerHTML='0'+0;
+    }
+    if(hours >60){
+        getHours.innerHTML=hours;
+    }*/
 }
